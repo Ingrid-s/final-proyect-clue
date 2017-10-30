b03c911938e9681250d1ba091a6835726a01e18b
ingridsag <starlling@hotmail.com>
2017-10-29 21:24:02 -0600

    cambios al codigo

---
+++ README.md
@@ -0,0 +1,6 @@
+## Pistas sobre proyecto final
+
+1.- repaso sobre métodos en arreglos
+2.- Métodos para las strings
+3.-Ascii
+4.- for, condicionales (if, if else);
---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*var testArray = [];
+for(var i = 0; i <= 10 ; i+=1){
+  /*Agregar elementos al arreglo en la última posición
+  testArray.push(i);
+  console.log (testArray);
+  // eliminar el primer elemento del array
+  testArray.shift();
+  console.log(testArray);
+  //Eliminar ultimo elemento del arreglo
+  testArray.pop();
+  console.log(testArray);
+  //Revertir los elementos del arreglo
+  testArray.reverse();
+  console.log(testArray);
+}
+
+/*---------------------------------------------------------*/
+var testString = "abcde";
+testString.charAt(2);
+/*para obtener un acaracter a partir de su codigo Ascii
+utilzo el metodo string.fromCharCode()
+para obtener el codogio ascii de un caracter utilizo String.chaCodeAt(indice)*/
+var codigo = testString.charCodeAt(2);
+console.log(codigo);
+// 99
+string.fromCharCode(codigo);
+// c
